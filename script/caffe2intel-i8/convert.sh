3726549dc5b44c0eaae84555c1e6f6f5738b8cca
Your Name <you@example.com>
2018-02-19 12:07:47 +0000

    meta and install script fixed

--- script/caffe2intel-i8/convert.sh
+++ script/caffe2intel-i8/convert.sh
@@ -16,16 +16,16 @@ echo "DEPLOY_BLOB_NAME=${DEPLOY_BLOB_NAME}"
 echo "TRAIN_VAL_BLOB_NAME=${TRAIN_VAL_BLOB_NAME}"
 
 function ConvertPrototxt() {
-  INPUT_PROTOTXT = $1
-  BLOB_NAME = $2
+  INPUT_PROTOTXT=$1
+  BLOB_NAME=$2
   echo
   echo "Convertion ${INPUT_PROTOTXT} ..."
   python ${CK_ENV_LIB_CAFFE}/../src/scripts/calibrator.py \
     --root=${CK_ENV_LIB_CAFFE} \
     --weights=${CK_ENV_MODEL_CAFFE_WEIGHTS} \
     --model=${INPUT_PROTOTXT} \
-    --iterations=${ITERATIONS}
-    --blob_name=${BLOB_NAME}
+    --iterations=${ITERATIONS} \
+    --blob_name="${BLOB_NAME}"
   if [ "${?}" != "0" ]
   then
     echo "Error: Convertion failed!"
@@ -34,7 +34,7 @@ function ConvertPrototxt() {
 }
 
 if [ -f ${INPUT_TRAIN_VAL_PROTOTXT} ]; then
-  #ConvertPrototxt ${INPUT_TRAIN_VAL_PROTOTXT} ${TRAIN_VAL_BLOB_NAME}
+  ConvertPrototxt ${INPUT_TRAIN_VAL_PROTOTXT} ${TRAIN_VAL_BLOB_NAME}
   cp ${INPUT_TRAIN_VAL_PROTOTXT} ${INSTALL_DIR}
 fi
 
@@ -44,6 +44,6 @@ if [ -f ${INPUT_DEPLOY_PROTOTXT} ]; then
 fi
 
 cp ${CK_ENV_MODEL_CAFFE}/${CK_ENV_MODEL_CAFFE_MEAN_BIN_FILE} ${INSTALL_DIR}
-cp ${CK_ENV_MODEL_CAFFE}/${CK_ENV_MODEL_CAFFE_WEIGHTS_FILE} ${INSTALL_DIR}
+#cp ${CK_ENV_MODEL_CAFFE}/${CK_ENV_MODEL_CAFFE_WEIGHTS_FILE} ${INSTALL_DIR}
 
 exit 0
